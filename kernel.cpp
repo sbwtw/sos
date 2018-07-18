8e9bc9476a9503d837161aba52186bc1d7ab1195
石博文 <sbw@sbw.so>
2018-07-18 20:12:06 +0800

    init

---
+++ kernel.cpp
@@ -0,0 +1,32 @@
+
+#include "types.h"
+
+void printf(char *str)
+{
+    static uint16_t *VideoMemory = (uint16_t *)0xb8000;
+
+    for (int i(0); str[i] != '\0'; ++i)
+    {
+        VideoMemory[i] = (VideoMemory[i] & 0xff00) | str[i];
+    }
+}
+
+typedef void (*constructor)();
+
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+
+extern "C" void callConstructors()
+{
+    for (constructor *i = &start_ctors; i != &end_ctors; ++i)
+    {
+        (*i)();
+    }
+}
+
+extern "C" void kernelMain(void *multiboot_structure, uint32_t magic_number)
+{
+    printf("sbw OS");
+
+    while (1);
+}
