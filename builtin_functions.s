622b2e1683f9de14739033a9f43a280ce6dec898
石博文 <sbw@sbw.so>
2018-07-21 22:27:57 +0800

    printf function support format template

--- builtin_functions.s
+++ builtin_functions.s
@@ -1,35 +1,120 @@
 
 .section .text
 .extern putc
-.global __sos_builtin_printf
+.global printf
 
-__sos_builtin_printf:
+printf:
     push %ebp
     mov %esp, %ebp
 
-    mov $0, %eax
-    mov $0, %ecx
+    sub $4, %esp
+    movl $8, -4(%ebp)
 
-    mov 8(%ebp), %esi
+    push %esi
+    push %edi
+
+    xor %eax, %eax
+    xor %ecx, %ecx
+
+    mov -4(%ebp), %edx
+    mov (%ebp, %edx), %esi
 
 process:
-    movb (%esi, %ecx), %al
+    movb (%esi, %ecx), %dl
 
-    test %al, %al
+    test %dl, %dl
     jz end
 
+    # test format character, 0x25 == '%'
+    cmpb $0x25, %dl
+    je format
+
+    push %ecx
+    push %eax
+    push %edx
+    call putc
+    #add $4, %esp
+    pop %eax
+    pop %ecx
+
+    inc %ecx
+    inc %eax
+    jmp process
+
+format:
+    inc %ecx
+    movb (%esi, %ecx), %dl
+
+    push %eax
+
+    # take arg
+    addl $4, -4(%ebp)
+    mov -4(%ebp), %eax
+    mov (%ebp, %eax), %eax
+
+    # digit, 0x64 == 'd'
+    cmpb $0x64, %dl
+    je format_digit
+
+    # char, 0x63 == 'c'
+    cmpb $0x63, %dl
+    je format_char
+
+    # string, 0x73 == 's'
+    cmpb $0x73, %dl
+    je format_string
+
+format_end:
+    pop %eax
+    jmp process
+
+format_digit:
+    inc %ecx
+
+    # 0x30 == '0'
+    add $0x30, %eax
+
+    push %ecx
+    push %eax
+    call putc
+    pop %ecx
+
+    jmp format_end
+
+format_char:
     inc %ecx
 
     push %ecx
     push %eax
     call putc
-    #add $4, %esp
     pop %ecx
 
-    jmp process
+    jmp format_end
+
+format_string:
+    inc %ecx
+    push %ecx
+
+format_string_next:
+    movb (%eax), %cl
+
+    test %cl, %cl
+    je format_string_end
+
+    push %eax
+    push %ecx
+    call putc
+    pop %eax
+    inc %eax
+    jmp format_string_next
+
+format_string_end:
+    pop %ecx
+    jmp format_end
 
 end:
-    mov %ecx, %eax
+    pop %edi
+    pop %esi
 
     mov %ebp, %esp
     pop %ebp
--- kernel.cpp
+++ kernel.cpp
@@ -3,7 +3,7 @@
 #include "gdt.h"
 #include "interrupts.h"
 
-extern "C" int __attribute__((cdecl)) __sos_builtin_printf(char *format);
+extern "C" int __attribute__((cdecl)) printf(char *format, ...);
 
 extern "C" void __attribute__((stdcall)) putc(char c)
 {
@@ -15,6 +15,10 @@ extern "C" void __attribute__((stdcall)) putc(char c)
 
     switch (c)
     {
+    case '\n':
+        ++y;
+        x = 0;
+        break;
     default:
         video_memory[cols * y + x] = (video_memory[cols * y + x] & 0xff00) | c;
         ++x;
@@ -22,49 +26,14 @@ extern "C" void __attribute__((stdcall)) putc(char c)
     }
 }
 
-void printf(char *str)
-{
-    static uint16_t *video_memory = (uint16_t *)0xb8000;
-    static uint8_t x(0), y(0);
+// void printf(char *str)
+// {
+    // __sos_builtin_printf(str);
 
-    const int cols = 80;
-    const int rows = 25;
-
-    for (int i(0); str[i] != '\0'; ++i)
-    {
-        switch (str[i])
-        {
-        case '\n':
-            x = 0;
-            ++y;
-            break;
-        default:
-            video_memory[cols * y + x] = (video_memory[cols * y + x] & 0xff00) | str[i];
-            ++x;
-            break;
-        }
-
-        if (x == 80)
-        {
-            ++y;
-            x = 0;
-        }
-
-        if (y == rows)
-        {
-            // clear screen
-            for (int idx(0); idx != cols * rows; ++idx)
-                video_memory[idx] = (video_memory[idx] & 0xff00) | ' ';
-
-            x = 0;
-            y = 0;
-        }
-    }
-
-    char *s = "kpwsdfasd";
-    int r = __sos_builtin_printf(s);
-    *((uint16_t *)0xb8000) = (*((uint16_t *)0xb8000) & 0xff00) | char(r + '0');
-}
+    // char *s = "test";
+    // int r = __sos_builtin_printf(s);
+    // *((uint16_t *)0xb8000) = (*((uint16_t *)0xb8000) & 0xff00) | char(r + '0');
+// }
 
 typedef void (*constructor)();
 
@@ -82,6 +51,9 @@ extern "C" void callConstructors()
 extern "C" void kernelMain(void *multiboot_structure, uint32_t magic_number)
 {
     printf("sbw's Operating System\n");
+    printf("number: %d - %d = %d\n", 6, 2, 6 - 2);
+    printf("char: %c\n", 'a');
+    printf("string: %s\n", "I'm string with zero terminated");
 
     GlobalDescriptorTable gdt;
     InterruptManager interrupts(&gdt);
